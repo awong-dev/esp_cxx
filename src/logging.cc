119debe7aa47100c3a0ad64a34d141cf7ee7aa8f
Albert J. Wong <awong.dev@gmail.com>
2019-12-23 00:33:34 -0800

    Add in syslog format

--- src/logging.cc
+++ src/logging.cc
@@ -1,5 +1,7 @@
 #include "esp_cxx/logging.h"
 
+#include <cassert>
+
 namespace esp_cxx {
 
 namespace {
@@ -7,10 +9,22 @@ namespace {
 static std::function<void(std::string_view)> g_on_log_cb;
 vprintf_like_t g_orig_vprintf;
 
-int FilterLogs(const char *format, va_list args) {
+int SyslogFormatFilter(const char *format, va_list args) {
   int retval = g_orig_vprintf(format, args);
+  time_t rawtime;
+  time (&rawtime);
+  struct tm *ptm = gmtime(&rawtime);
+  size_t pos = 0;
   static char buf[512];
-  vsnprintf(&buf[0], sizeof(buf), format, args);
+
+  // Format: <22>Oct 12 13:49:10 [devid] ledstrip "
+  pos += strftime(&buf[pos], sizeof(buf) - pos, "<22>%FT%TZ ", ptm);
+  assert(pos < sizeof(buf));
+
+  pos += snprintf(&buf[pos], sizeof(buf) - pos, "%s ledstrip ", "device_id"); // TODO(awong): Device id here.
+  assert(pos < sizeof(buf));
+
+  vsnprintf(&buf[pos], sizeof(buf) - pos, format, args);
   g_on_log_cb(buf);
 
   return retval;
@@ -21,7 +35,7 @@ int FilterLogs(const char *format, va_list args) {
 void SetLogFilter(std::function<void(std::string_view)> on_log) {
   g_on_log_cb = std::move(on_log);
   if (!g_orig_vprintf) {
-    g_orig_vprintf = esp_log_set_vprintf(&FilterLogs);
+    g_orig_vprintf = esp_log_set_vprintf(&SyslogFormatFilter);
   }
 }
 
